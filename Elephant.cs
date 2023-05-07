db7cac34cbf95ca416df10bee831c87970d27d99
Eduardo Salinas <eduardo_salinas_@outlook.com>
2023-05-06 22:07:15 -0700

    Add project files.

---
+++ Elephant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceVariables
+{
+    internal class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"My name is {Name}.\nMy ears are {EarSize} inches tall.");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿//main
+using ReferenceVariables;
+
+
+Elephant lucinda = new Elephant() { EarSize = 33, Name = "Lucinda" };
+Elephant lloyd = new Elephant() { EarSize = 40, Name = "Lloyd" };
+
+Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+
+while (true)
+{
+    string choice = Console.ReadLine();
+
+    if (choice == "1")
+    {
+        Console.WriteLine("You pressed 1\nCalling lloyd.WhoAmI");
+        lloyd.WhoAmI();
+
+    }
+    else if (choice == "2")
+    {
+        Console.WriteLine("You pressed 2\nCalling lucinda.WhoAmI");
+        lloyd.WhoAmI();
+    }
+    else if (choice == "3")
+    {
+        Elephant tempElephant = lucinda;
+        lloyd = tempElephant;
+        lucinda = lloyd;
+     
+        Console.WriteLine("You pressed 3\nReferences have been swapped");
+        
+    }
+    else
+    {
+        Console.WriteLine("Your entry was invalid, press space to try again or enter to quit");
+        string chooseAgain =  Console.ReadLine();
+        if (chooseAgain == "") { return; }
+        else { Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap"); }
+
+    }
+}
+
+
